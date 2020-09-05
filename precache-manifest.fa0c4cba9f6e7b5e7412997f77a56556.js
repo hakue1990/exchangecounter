a6ec57758242fe8df0c17a3a7349b67907fc99e8
Hakue <adam.haldas@gmail.com>
2020-09-05 14:04:48 +0200

    Updates

---
+++ precache-manifest.fa0c4cba9f6e7b5e7412997f77a56556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b669b90b8ef1f3b3f9dc68b6e01f4d18",
+    "url": "/exchangecounter/index.html"
+  },
+  {
+    "revision": "3b7752c5537d993f001c",
+    "url": "/exchangecounter/static/css/main.aa8c741c.chunk.css"
+  },
+  {
+    "revision": "52c024eb4bbc42c495e5",
+    "url": "/exchangecounter/static/js/2.7f557e25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchangecounter/static/js/2.7f557e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b7752c5537d993f001c",
+    "url": "/exchangecounter/static/js/main.fa68822a.chunk.js"
+  },
+  {
+    "revision": "4fce56c9ebc767bd887f",
+    "url": "/exchangecounter/static/js/runtime-main.45f13b6b.js"
+  },
+  {
+    "revision": "b6191516d286066c41c9d8062924b189",
+    "url": "/exchangecounter/static/media/github.b6191516.svg"
+  },
+  {
+    "revision": "3631f92ba0ad0233326c400e1d1bdbb2",
+    "url": "/exchangecounter/static/media/logo.3631f92b.svg"
+  }
+]);
